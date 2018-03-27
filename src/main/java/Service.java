6c714c12e69f17a79fcd13eed781668af1e861e5
Mafdmoore <mafdmoore@gmail.com>
2018-03-27 17:11:22 +0100

    print-account-as-json-branch comments fixed.

--- src/main/java/Service.java
+++ src/main/java/Service.java
@@ -4,22 +4,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Service
 {
-	//ATTRIBUTES
 	private HashMap<Integer, Account> m_accountMap;
 	private int m_key;
 	
 	
-	//METHODS
-	
-	//Constructor
 	public Service()
 	{
 		m_accountMap = new HashMap<Integer, Account>();
 		m_key = 0;
 	}
 	
-	
-	//Add and remove account
+
 	public void addAccount(String firstname, String lastname, int accountnumber)
 	{
 		m_accountMap.put(m_key, new Account(firstname, lastname, accountnumber));
@@ -31,13 +26,11 @@ public class Service
 		if(m_accountMap.containsKey(key)) m_accountMap.remove(key);
 	}
 	
-	//Retrieve account
 	public Account retrieveAccount(int key)
 	{
 		return m_accountMap.get(key);
 	}
 
-	//Translate hashmap to JSON
 	public String translateAccountMap() throws JsonProcessingException
 	{
 		ObjectMapper mapper = new ObjectMapper();
--- src/main/java/WinMain.java
+++ src/main/java/WinMain.java
@@ -1,19 +1,14 @@
 public class WinMain
 {	
-	//ATTRIBUTES
 	Service m_service;
 	
 	
-	//METHODS
-	
-	//Constructor
 	public WinMain()
 	{
 		m_service = new Service();
 	}
 	
 	
-	//main
 	public static void main(String[] args)
 	{
 		WinMain winmain = new WinMain();
